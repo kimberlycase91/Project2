d84747b1108e0999ad2cac4b3dbbf9abb7dc6e36
kimberlycase91 <kimberlycase91@gmail.com>
2020-03-24 20:18:05 -0700

    re-did ajax call to new api

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Get references to page elements
   var gameTitle = $("#gameTitle");
   var gameSummary = $("#gameSummary");
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   // The API object contains methods for each kind of request we'll make
   var API = {
-    postReview: function() {
+    postReview: function () {
       return $.ajax({
         headers: {
           "Content-Type": "application/json"
@@ -20,24 +20,24 @@ $(document).ready(function() {
         data: JSON.stringify()
       });
     },
-    getReview: function() {
-      return  $.ajax({
+    getReview: function () {
+      return $.ajax({
         url: "api/review",
         type: "GET"
-      }); 
-  },
-  deleteReview: function(id) {
-    return $.ajax({
-      url: "api/review/" + id,
-      type: "DELETE"
+      });
+    },
+    deleteReview: function (id) {
+      return $.ajax({
+        url: "api/review/" + id,
+        type: "DELETE"
       });
     }
   };
 
   // refreshExamples gets new examples from the db and repopulates the list
-  var refreshReviews = function() {
-    API.getReview().then(function(data) {
-      var reviews = data.map(function(review) {
+  var refreshReviews = function () {
+    API.getReview().then(function (data) {
+      var reviews = data.map(function (review) {
         var gameID = $(("#search").val().join().toLowerCase());
         var $a = $("<a>")
           .text(review.text)
@@ -49,23 +49,23 @@ $(document).ready(function() {
             "data-id": review.id
           })
           .append($a);
-  
+
         var $button = $("<button>")
           .addClass("btn btn-danger float-right delete")
           .text("ｘ");
-  
+
         $li.append($button);
-  
+
         return $li;
       });
-     
+
       reviewList.empty();
       reviewList.append(reviews);
     });
   };
   // handleFormSubmit is called whenever we submit a new example
   // Save the new example to the db and refresh the list
-  var handleFormSubmit = function(event) {
+  var handleFormSubmit = function (event) {
     event.preventDefault();
 
     var gameInfo = {
@@ -79,7 +79,7 @@ $(document).ready(function() {
       return;
     }
 
-    API.postReview(example).then(function() {
+    API.postReview(example).then(function () {
       refreshReviews();
     });
 
@@ -90,12 +90,12 @@ $(document).ready(function() {
 
   // handleDeleteBtnClick is called when an example's delete button is clicked
   // Remove the example from the db and refresh the list
-  var handleDeleteBtnClick = function() {
+  var handleDeleteBtnClick = function () {
     var idToDelete = $(this)
       .parent()
       .attr("data-id");
 
-    API.deleteReview(idToDelete).then(function() {
+    API.deleteReview(idToDelete).then(function () {
       refreshReviews();
     });
   };
@@ -103,31 +103,33 @@ $(document).ready(function() {
   var $submit = $("#submit");
   var $results = $("#resultBox");
   var $gameInfo = $("#gameInfo");
-  var APIkey = "278971f36b31bc8cf08118637d493047289c1aaa" //put this string in .env as APIkey="278971f36b31bc8cf08118637d493047289c1aaa" then change the value here to process.env.APIkey
-  var API_URL = "https://www.gamespot.com/api/games/?api_key=" + APIkey + "&format=json&limit=10&sort=release_date:dsc&filter=name:"
+  var APIkey = "0c95b8af9amshc7c1d9d6af83ea3p12fefbjsn5bf38f4be1cc" //put this string in .env as APIkey="278971f36b31bc8cf08118637d493047289c1aaa" then change the value here to process.env.APIkey
 
-  $("#submit").on("click", function (){
-  event.preventDefault();
-  console.log("searching for game...");
-  console.log($("#game-input").val());
-  gameName = $("#game-input").val().trim()
-  $.ajax({
-    url: API_URL + gameName,
-    conentType: "application/json",
-    dataType: "jsonp",
-      success: function (data) {
-        console.log(data);
-        for (var i=0; i < data.results.length; i++) {
-            var newResult = ("<div>");
-            var gameName = ("<h3>");
-            gameName.text(data.results[i].name);
-            newResult.append(gamename);
-            var gameSummary = ("<p>");
-            gameSummary.text(data.results[i].description);
-            newResult.append(gameSummary);
-            $results.append(newResult);
-         }
-      }
-    })
-  })
+
+  $("#submit").on("click", function () {
+    event.preventDefault();
+    console.log("searching for game...");
+    console.log($("#game-input").val());
+    var gameSearch = $("#game-input").val().trim();
+    var API_URL = "https://api.rawg.io/api/games/" + gameSearch;
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": API_URL,
+      "method": "GET",
+    };
+    console.log(API_URL);
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      console.log(response.name);
+      var newResult = $("<div>");
+      var gameName = $("<h3>");
+      gameName.text(response.name);
+      newResult.append(gameName);
+      var gameSummary = $("<div>");
+      gameSummary.text(response.description);
+      newResult.append(gameSummary);
+      $results.append(newResult);
+    });
+  });
 });
